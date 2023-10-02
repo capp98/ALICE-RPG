a68f1e4460b7e992ffb34504aae8edb695fdf9e5
César <54820067+capp98@users.noreply.github.com>
2023-10-02 20:53:46 -0300

    Update index.js

--- components/diario/index.js
+++ components/diario/index.js
@@ -1,24 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function Diario({ ficha }) {
-  let { reliquias } = ficha;
-  let reliquiasComAtributos = reliquias.filter(
-    (reliquia) => !!reliquia.atributos
-  );
+export default function Diario({ personagem_id }) {
+  const [diarios, setDiarios] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://anxious-puce-cloak.cyclic.cloud/diario/${personagem_id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDiarios(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <h1>Carregando...</h1>;
+
   return (
     <div className="infos-window" id="diario">
-      <a>
-        <h1>Sessão</h1>
-        <h4>01/10/2023</h4>
-      </a>
-      <a>
-        <h1>Sessão</h1>
-        <h4>01/10/2023</h4>
-      </a>
-      <a>
-        <h1>Sessão</h1>
-        <h4>01/10/2023</h4>
-      </a>
+      {diarios.map((diario, d) => (
+        <article key={d}>
+          <h1>{diario.titulo}</h1>
+          <h4>{diario.data}</h4>
+          <section dangerouslySetInnerHTML={{ __html: diario.cena }}></section>
+        </article>
+      ))}
     </div>
   );
 }
