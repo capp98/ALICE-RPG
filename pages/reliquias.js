9e5066febe328db6907cd6b7016d96878cb52af5
César <54820067+capp98@users.noreply.github.com>
2023-10-15 16:55:49 -0300

    Update reliquias.js

--- pages/reliquias.js
+++ pages/reliquias.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 export default function Reliquias() {
   const [reliquias, setReliquias] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [urlImagem, setUrlImagem] = useState();
 
   useEffect(() => {
     fetch(`https://anxious-puce-cloak.cyclic.cloud/reliquias`)
@@ -16,40 +17,51 @@ export default function Reliquias() {
   if (isLoading) return <h1>Carregando</h1>;
 
   return (
-    <table id="inventario-reliquias">
-      <thead>
-        <tr>
-          <th></th>
-          <th>Nome</th>
-          <th>Tag</th>
-          <th>Efeito</th>
-        </tr>
-      </thead>
-      <tbody>
-        {reliquias.map((reliquia, index) =>
-          reliquia.tag.map((tag, tagIndex) => (
-            <tr key={index + '-' + tagIndex}>
-              {tagIndex === 0 ? (
-                <>
-                  <td rowSpan={reliquia.tag.length}>
-                    <img
-                      src={`/static/images/reliquias/${reliquia.nome.toLowerCase()}.jpg`}
-                      alt={reliquia.nome}
-                    />
-                  </td>
-                  <td rowSpan={reliquia.tag.length}>{reliquia.nome}</td>
-                </>
-              ) : null}
-              <td>{tag}</td>
-              <td>
-                {!!reliquia.tag_descricao
-                  ? reliquia.tag_descricao[tagIndex]
-                  : ''}
-              </td>
-            </tr>
-          ))
-        )}
-      </tbody>
-    </table>
+    <>
+      <table id="inventario-reliquias">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Tag</th>
+            <th>Efeito</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reliquias.map((reliquia, index) =>
+            reliquia.tag.map((tag, tagIndex) => (
+              <tr key={index + '-' + tagIndex}>
+                {tagIndex === 0 ? (
+                  <>
+                    <td rowSpan={reliquia.tag.length}>
+                      <img
+                        onClick={() =>
+                          setUrlImagem(
+                            `/static/images/reliquias/${reliquia.nome.toLowerCase()}.jpg`
+                          )
+                        }
+                        src={`/static/images/reliquias/${reliquia.nome.toLowerCase()}.jpg`}
+                        alt={reliquia.nome}
+                      />
+                      {reliquia.nome}
+                    </td>
+                  </>
+                ) : null}
+                <td>{tag}</td>
+                <td>
+                  {!!reliquia.tag_descricao
+                    ? reliquia.tag_descricao[tagIndex]
+                    : ''}
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+      {!!urlImagem && (
+        <div onClick={() => setUrlImagem()} id="imagem">
+          <img src={urlImagem} />
+        </div>
+      )}
+    </>
   );
 }
