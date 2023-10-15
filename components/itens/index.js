b76cf28c3a51804754e82934f1a585bec674e7a3
César <54820067+capp98@users.noreply.github.com>
2023-10-15 18:50:43 -0300

    Update index.js

--- components/itens/index.js
+++ components/itens/index.js
@@ -1,35 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function Itens({ ficha }) {
-  let { reliquias } = ficha;
+export default function Itens({ personagem_id }) {
+  const [reliquias, setReliquias] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://anxious-puce-cloak.cyclic.cloud/reliquias/${personagem_id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setReliquias(data);
+        setIsLoading(false);
+      });
+  }, [personagem_id]);
+
+  if (isLoading)
+    return (
+      <h1 style={{ textAlign: 'center', marginTop: '50px' }}>Buscando Itens</h1>
+    );
+
   let reliquiasComAtributos = reliquias.filter(
     (reliquia) => !!reliquia.atributos
   );
   return (
-    <table>
-      <thead>
-        <tr>
-          <th colSpan="2">Itens</th>
-        </tr>
-        <tr>
-          <th>Item</th>
-          <th>Buff</th>
-        </tr>
-      </thead>
-      <tbody id="table-itens">
-        {reliquiasComAtributos.map((reliquia, i) => (
-          <tr key={i}>
-            <td>{reliquia.nome}</td>
-            <td>
-              {Object.entries(reliquia.atributos).map(([atributo, valor]) => (
-                <p key={atributo}>
-                  <strong>{atributo}:</strong> {valor}
-                </p>
-              ))}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="infos-window" id="reliquias">
+      <table>
+        <tbody id="table-itens">
+          {reliquiasComAtributos.map((item, index) => (
+            <>
+              <tr className="reliquiaNome">
+                <td>{item.nome}</td>
+              </tr>
+              {Object.entries(item.atributos).map(
+                ([atributo, valor], atIndex) =>
+                  valor.length > 0 ? (
+                    valor.map((v, i) => (
+                      <tr>
+                        <td key={i}>{v}</td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr key={atributo}>
+                      <td>
+                        <strong>{atributo}:</strong> {valor}
+                      </td>
+                    </tr>
+                  )
+              )}
+            </>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
