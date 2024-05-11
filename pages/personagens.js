ca6dc6c069c6ca62e8f13b0220f77392806da33b
capp98 <54820067+capp98@users.noreply.github.com>
2024-05-11 13:12:35 -0300

    Dados adicionados novamente

--- pages/personagens.js
+++ pages/personagens.js
@@ -1,8 +1,76 @@
-export default function Status() {
+import { useState, useEffect } from 'react';
+
+export default function Reliquias() {
+  const [reliquias, setReliquias] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [urlImagem, setUrlImagem] = useState();
+
+  useEffect(() => {
+    fetch(`https://anxious-puce-cloak.cyclic.cloud/reliquias`)
+      .then((response) => response.json())
+      .then((data) => {
+        setReliquias(data.filter((reliquia) => !!reliquia.tag));
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <h1>Carregando</h1>;
+
+  return (
+    <>
+      <table id="inventario-reliquias">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Tag</th>
+            <th>Efeito</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reliquias.map((reliquia, index) =>
+            reliquia.tag.map((tag, tagIndex) => (
+              <tr key={index + '-' + tagIndex}>
+                {tagIndex === 0 ? (
+                  <>
+                    <td rowSpan={reliquia.tag.length}>
+                      <img
+                        onClick={() =>
+                          setUrlImagem(
+                            `/static/images/reliquias/${reliquia.nome.toLowerCase()}.jpg`
+                          )
+                        }
+                        src={`/static/images/reliquias/${reliquia.nome.toLowerCase()}.jpg`}
+                        alt={reliquia.nome}
+                      />
+                      {reliquia.nome}
+                    </td>
+                  </>
+                ) : null}
+                <td>{tag}</td>
+                <td>
+                  {!!reliquia.tag_descricao
+                    ? reliquia.tag_descricao[tagIndex]
+                    : ''}
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+      {!!urlImagem && (
+        <div onClick={() => setUrlImagem()} id="imagem">
+          <img src={urlImagem} />
+        </div>
+      )}
+    </>
+  );
+}
+
+/*export default function Status() {
   return (
     <h1 style={{ textAlign: 'center', marginTop: '50px' }}>
       {' '}
       Não há personagens para serem lembrados.
     </h1>
   );
-}
+}*/
--- pages/status.js
+++ pages/status.js
@@ -1,7 +1,8 @@
 export default function Status() {
   return (
     <h1 style={{ textAlign: 'center', marginTop: '50px' }}>
-      Não há e nunca houve história para ser contada
+      {' '}
+      Ops. Parece que o café acabou antes de eu terminar isso aqui 💀
     </h1>
   );
 }
